860f936d07f6416b80da44330eb7f05ebbc61cc1
Arjun Dhillon <mail2arjund@icloud.com>
2026-04-26 13:35:45 -0600

    Patch: Fixed Vite Config for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Citewell/', // This ensures paths work on GitHub Pages
 })
